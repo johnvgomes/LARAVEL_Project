2b0a77eb76184ffb66be96a063879960ffa6d26f
denius72 <denius72@gmail.com>
2017-11-16 08:20:54 -0200

    Atualizando rotas para padrão Rest

--- ProjetoIrineu/routes/web.php
+++ ProjetoIrineu/routes/web.php
@@ -21,56 +21,18 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 ###################     rotas do cliente     #######################
 
-Route::get('clientes', 'ClientesController@index');
-
-Route::get('clientes/{cliente}/editar', 'ClientesController@editar');
-
-Route::get('clientes/novo', 'ClientesController@novo');
-
-Route::get('clientes/{cliente}/confirmDelete', 'ClientesController@confirmarDelete');
-
-Route::post('clientes/salvar', 'ClientesController@salvar');
-
-Route::patch('clientes/{cliente}', 'ClientesController@atualizar');
-
-Route::delete('clientes/{cliente}', 'ClientesController@deletar');
+Route::resource('clientes', 'ClientesController');
 
 #################### ###################### #######################
 
 ###################     rotas da categoria  #######################
 
-
-Route::get('categories', 'CategoriesController@index');
-
-Route::get('categories/{category}/editar', 'CategoriesController@editar');
-
-Route::get('categories/novo', 'CategoriesController@novo');
-
-Route::get('categories/{category}/confirmDelete', 'CategoriesController@confirmarDelete');
-
-Route::post('categories/salvar', 'CategoriesController@salvar');
-
-Route::patch('categories/{category}', 'CategoriesController@atualizar');
-
-Route::delete('categories/{category}', 'CategoriesController@deletar');
+Route::resource('categories', 'CategoriesController');
 
 #################### ###################### #######################
 
 ###################     rotas do produto  #######################
 
-
-Route::get('products', 'ProductsController@index');
-
-Route::get('products/novo', 'ProductsController@novo');
-
-Route::get('products/{product}/editar', 'ProductsController@editar');
-
-Route::get('products/{product}/confirmDelete', 'ProductsController@confirmarDelete');
-
-Route::post('products/salvar', 'ProductsController@salvar');
-
-Route::patch('products/{product}', 'ProductsController@atualizar');
-
-Route::delete('products/{product}', 'ProductsController@deletar');
+Route::resource('products', 'ProductsController');
 
 #################### ###################### #######################
