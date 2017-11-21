86d4ab311759503aecff283ce72c926c3ed3e3f0
denius72 <denius72@gmail.com>
2017-11-21 21:15:36 -0200

    euhh

--- ProjetoIrineu/resources/views/layouts/app.blade.php
+++ ProjetoIrineu/resources/views/layouts/app.blade.php
@@ -6,9 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
+    <link href="/Laravel/ProjetoIrineu/public/css/mui.min.css" rel="stylesheet" type="text/css" />
+    <script src="/Laravel/ProjetoIrineu/public/js/mui.min.js"></script>
+
     <link href="/css/mui.min.css" rel="stylesheet" type="text/css" />
     <script src="/js/mui.min.js"></script>
-    <!-- olha elaaaaaaaaaaaaaaaaaaa -->
     <!-- CSRF Token -->
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
