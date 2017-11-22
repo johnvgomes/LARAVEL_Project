5350bad89efa82116ee9c61aab8ce429cdfa86be
Leonardo Geremia <Geremaia@localhost.localdomain>
2017-11-22 11:08:48 -0200

    arrumando controlers de inscrição e de processo seletivo

--- ProjetoIrineu/app/Http/Controllers/SelectiveProcessController.php
+++ ProjetoIrineu/app/Http/Controllers/SelectiveProcessController.php
@@ -9,5 +9,113 @@ use Redirect;
 
 class SelectiveProcessController extends Controller
 {
-    
+    public function index()
+    {
+        $selectiveprocesses = SelectiveProcess::all();
+       
+
+        return view('selectiveprocess.lista',['selectiveprocesses'=> $selectiveprocesses]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    { 
+        return view('selectiveprocess.formulario');
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+       
+          $selectiveprocesses = new SelectiveProcess();
+        
+          $selectiveprocesses = $selectiveprocesses->create($request->all());
+
+
+          \Session::flash('mensagem_sucesso', 'processo seletivo criado com sucesso!');
+
+          return Redirect::to('selectiveprocesses/create');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $selectiveprocesses = SelectiveProcess::findorfail($id);
+        
+        return view('selectiveprocesses.formulario',['selectiveprocesses'=> $selectiveprocesses]);
+          
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $selectiveprocesses = SelectiveProcess::findorfail($id);
+        
+        $selectiveprocesses->update($request->all());
+
+        \Session::flash('mensagem_sucesso', 'Processo seletivo atualizado com sucesso!');
+        
+         return Redirect::to('selectiveprocesses/'.$selectiveprocesses->id.'/edit');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        
+       $selectiveprocesses = SelectiveProcess::findorfail($id);
+       
+       $selectiveprocesses->delete();
+
+      \Session::flash('mensagem_sucesso', 'Processo seletivo deletado com sucesso!');
+      
+       return Redirect::to('selectiveprocesses');
+
+    }
+
+    public function confirmdestroy($id)
+    {
+        
+        $selectiveprocesses = SelectiveProcess::findorfail($id);
+        
+        return view('selectiveprocesses.deleteConfirm',['selectiveprocesses'=> $selectiveprocesses]);
+
+
+    }
 }
--- ProjetoIrineu/app/Http/Controllers/SubscriptionController.php
+++ ProjetoIrineu/app/Http/Controllers/SubscriptionController.php
@@ -9,5 +9,113 @@ use Redirect;
 
 class SubscriptionController extends Controller
 {
-    
+    public function index()
+    {
+        $subscriptions = Subscription::all();
+       
+
+        return view('subscriptions.lista',['subscriptions'=> $subscriptions]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    { 
+        return view('subscriptions.formulario');
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+       
+          $subscriptions = new Subscription();
+        
+          $subscriptions = $subscriptions->create($request->all());
+
+
+          \Session::flash('mensagem_sucesso', 'Inscrição feita com sucesso!');
+
+          return Redirect::to('subscriptions/create');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $subscriptions = Subscription::findorfail($id);
+        
+        return view('subscriptions.formulario',['subscriptions'=> $subscriptions]);
+          
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $subscriptions = Subscription::findorfail($id);
+        
+        $subscriptions->update($request->all());
+
+        \Session::flash('mensagem_sucesso', 'Inscrição atualizada com sucesso!');
+        
+         return Redirect::to('subscriptions/'.$subscriptions->id.'/edit');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        
+       $subscriptions = Subscription::findorfail($id);
+       
+       $subscriptions->delete();
+
+      \Session::flash('mensagem_sucesso', 'Inscrição deletada com sucesso!');
+      
+       return Redirect::to('subscriptions');
+
+    }
+
+    public function confirmdestroy($id)
+    {
+        
+        $subscriptions = Subscription::findorfail($id);
+        
+        return view('subscriptions.deleteConfirm',['subscriptions'=> $subscriptions]);
+
+
+    }
 }
