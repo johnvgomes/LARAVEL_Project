a14d26e9e23f6fc88bdc86d85a9b5090929ca102
Leonardo Geremia <Geremaia@localhost.localdomain>
2017-11-22 10:45:40 -0200

    criando controllers de processo seletivo e inscrição

---
+++ ProjetoIrineu/app/Http/Controllers/SelectiveProcessController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SelectiveProcess;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Redirect;
+
+class SelectiveProcessController extends Controller
+{
+    
+}
---
+++ ProjetoIrineu/app/Http/Controllers/SubscriptionController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Subscription;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Redirect;
+
+class SubscriptionController extends Controller
+{
+    
+}
