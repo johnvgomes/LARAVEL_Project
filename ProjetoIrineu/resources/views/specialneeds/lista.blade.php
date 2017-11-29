81886f4e02ba7af323327b5173bad6dcd7f810b2
denius72 <denius72@gmail.com>
2017-11-29 13:07:30 -0200

    Melhorado responsividade

--- ProjetoIrineu/resources/views/specialneeds/lista.blade.php
+++ ProjetoIrineu/resources/views/specialneeds/lista.blade.php
@@ -10,40 +10,41 @@
                 @if(Session::has('mensagem_sucesso'))
                   <div class= "alert alert-success">{{ Session::get('mensagem_sucesso')}}</div>
                 @endif
-
-                <table class ="mui-table" style="margin-top: -35px;">
-                    <thead >
-                        <tr>
-                            <th>
-                                <div style="margin-top: 20px;">
-                                    Descrição
-                                </div>
-                            </th>
-                            <th>
-                                <form action="{{url('specialneeds/create')}}" style="margin-left: 100px; margin-bottom: -20px;">
-                                    <button type="submit" class="mui-btn mui-btn--small mui-btn--primary mui-btn--flat" style="left: 60%;">Nova Necessidade Especial</button>
-                                </form>
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    @foreach ($spneeds as $spneed)
-                    <tr style="border-top: 1px solid #E0E0E0; border-collapse: separate;">
-                        <td>{{ $spneed->description }}</td>
-                        <td style="display: flex; margin-left: 83%;">
-                            <form action="specialneeds/{{ $spneed->id }}/edit">
-                                <button class="mui-btn mui-btn--small mui-btn--raised mui-btn--primary" style="display: inline; font-size: 25;"><img src="/icon/ic_mode_edit_white_24px.svg" height="20" width="20"/></button>
-                            </form>    
-                        </td> 
-                        <td style="margin-left: 30px;">                
-                            <form action="specialneeds/{{ $spneed->id }}/confirmDestroy">
-                                <button class="mui-btn mui-btn--small mui-btn--raised mui-btn--danger" style="display: inline; font-size: 20;"><img src="/icon/ic_delete_forever_white_24px.svg" height="20" width="20" /></button>
-                            </form>
-                        </td>
-                    </tr>
-                    @endforeach
-                    </tbody>
-                </table>
+                    <div style="margin-left: calc(30% - 150px);">
+                        <table class ="mui-table" style="margin-top: -35px;">
+                            <thead>
+                                <tr>
+                                    <th>
+                                        <div style="margin-top: 20px;">
+                                            Descrição
+                                        </div>
+                                    </th>
+                                    <th>
+                                        <div style="margin-top: 20px; margin-left: calc(100% + 75px);">
+                                            Ações
+                                        </div>
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            @foreach ($spneeds as $spneed)
+                            <tr style="border-top: 1px solid #E0E0E0; border-collapse: separate;">
+                                <td>{{ $spneed->description }}</td>
+                                <td>
+                                    <form action="specialneeds/{{ $spneed->id }}/edit" style="margin-top: 10px; margin-left: calc(100% + 40px);">
+                                        <button class="mui-btn mui-btn--small mui-btn--raised mui-btn--primary" style="display: inline; font-size: 25;"><img src="/icon/ic_mode_edit_white_24px.svg" height="20" width="20"/></button>
+                                    </form>    
+                                </td> 
+                                <td>                
+                                    <form action="specialneeds/{{ $spneed->id }}/confirmDestroy" style="margin-top: 10px; margin-left: calc(10% + 70px);">
+                                        <button class="mui-btn mui-btn--small mui-btn--raised mui-btn--danger" style="display: inline; font-size: 20;"><img src="/icon/ic_delete_forever_white_24px.svg" height="20" width="20" /></button>
+                                    </form>
+                                </td>
+                            </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
@@ -51,9 +52,6 @@
 </div>
 <form action="{{url('specialneeds/create')}}" style="margin-left: 100px; margin-bottom: -20px;">
     <div class="fixed-action-btn horizontal click-to-toggle">
-    <button class="mui-btn mui-btn--fab mui-btn--primary">+</button>
-        <ul>
-            <li><a class="btn-floating green"><i class="material-icons">insert_chart</i></a></li>
-        </ul>
+        <button class="mui-btn mui-btn--fab mui-btn--primary">+</button>
     </div>
 </form>
