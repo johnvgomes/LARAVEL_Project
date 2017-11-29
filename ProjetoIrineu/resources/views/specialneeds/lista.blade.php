b87706c7ee4d78f1041b93a3a0eecedb71293ab9
denius72 <denius72@gmail.com>
2017-11-29 16:03:23 -0200

    Lista de processos seletivos ao Material Design

--- ProjetoIrineu/resources/views/specialneeds/lista.blade.php
+++ ProjetoIrineu/resources/views/specialneeds/lista.blade.php
@@ -6,7 +6,7 @@ Necessidades Especiais
 @endsection
 <div class="mui-container">
     <div class="row">
-        <div class="panel-body" style="width: 50%; left:25%; right: 75%; position: absolute;">
+        <div style="width: 50%; left:25%; right: 75%; position: absolute;">
                 
                 <div class="panel-body">
 
@@ -14,42 +14,41 @@ Necessidades Especiais
                   <div class= "alert alert-success">{{ Session::get('mensagem_sucesso')}}</div>
                 @endif
                     <div style="margin-left: calc(30% - 150px);">
-                        <table class ="mui-table" style="margin-top: -35px;">
-                            <thead style="border-bottom: 2px solid #E0E0E0;">
-                                <tr>
-                                    <th>
-                                        <div style="margin-top: 20px;">
-                                            Descrição
-                                        </div>
-                                    </th>
-                                    <th>
-                                        <div style="margin-top: 20px; margin-left: calc(100% + 40px);">
-                                            Ações
-                                        </div>
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                            @foreach ($spneeds as $spneed)
-                            <tr style="border-top: 1px solid #E0E0E0; border-collapse: separate;">
-                                <td>{{ $spneed->description }}</td>
-                                <td>
-                                    <form action="specialneeds/{{ $spneed->id }}/edit" style="margin-top: 10px; margin-left: calc(100% + 40px);">
-                                        <button class="mui-btn mui-btn--small mui-btn--raised mui-btn--primary" style="display: inline; font-size: 25;"><img src="/icon/ic_mode_edit_white_24px.svg" height="20" width="20"/></button>
-                                    </form>    
-                                </td> 
-                                <td>                
-                                    <form action="specialneeds/{{ $spneed->id }}/confirmDestroy" style="margin-top: 10px; margin-left: calc(10% + 70px);">
-                                        <button class="mui-btn mui-btn--small mui-btn--raised mui-btn--danger" style="display: inline; font-size: 20;"><img src="/icon/ic_delete_forever_white_24px.svg" height="20" width="20" /></button>
-                                    </form>
-                                </td>
-                            </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
+                            <table class ="mui-table" style="margin-top: 0px;">
+                                <thead style="border-bottom: 2px solid #E0E0E0;">
+                                    <tr>
+                                        <th>
+                                            <div style="margin-top: 20px;">
+                                                Descrição
+                                            </div>
+                                        </th>
+                                        <th>
+                                            <div style="margin-top: 20px; margin-left: calc(100% - 70px);">
+                                                Ações
+                                            </div>
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($spneeds as $spneed)
+                                        <tr style="border-top: 1px solid #E0E0E0; border-collapse: separate;">
+                                            <td>{{ $spneed->description }}</td>
+                                            <td>
+                                                <form action="specialneeds/{{ $spneed->id }}/edit" style="margin-top: 10px; margin-left: calc(100% - 70px);">
+                                                    <button class="mui-btn mui-btn--small mui-btn--raised mui-btn--primary" style="display: inline; font-size: 25;"><img src="/icon/ic_mode_edit_white_24px.svg" height="20" width="20"/></button>
+                                                </form>    
+                                            </td> 
+                                            <td>                
+                                                <form action="specialneeds/{{ $spneed->id }}/confirmDestroy" style="margin-top: 10px; margin-left: calc(10% - 35px);">
+                                                    <button class="mui-btn mui-btn--small mui-btn--raised mui-btn--danger" style="display: inline; font-size: 20;"><img src="/icon/ic_delete_forever_white_24px.svg" height="20" width="20" /></button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
                     </div>
                 </div>
-            </div>
         </div>
     </div>
 </div>
