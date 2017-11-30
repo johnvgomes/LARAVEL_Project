1901864899bfabe6d72db61aad2522965bcd479d
denius72 <denius72@gmail.com>
2017-11-30 06:26:46 -0200

    .

--- ProjetoIrineu/resources/views/home.blade.php
+++ ProjetoIrineu/resources/views/home.blade.php
@@ -1,20 +1,12 @@
 @extends('layouts.app')
 <br /><br /><br /><br />
 <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                </div>
-            </div>
+    @if (session('status'))
+        <div class="alert alert-success">
+            {{ session('status') }}
         </div>
-    </div>
+    @endif
+    <?php    
+    header("Location: ../index.php");exit;   
+    ?>
 </div>
