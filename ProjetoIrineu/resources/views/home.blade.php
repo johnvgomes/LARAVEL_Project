a38a86ead61926004363dcb3ed79de0fbd722d5f
denius72 <denius72@gmail.com>
2017-12-05 12:28:54 -0200

    Adicionado princípio do Dashboard

--- ProjetoIrineu/resources/views/home.blade.php
+++ ProjetoIrineu/resources/views/home.blade.php
@@ -1,12 +1,23 @@
 @extends('layouts.app')
 <br /><br /><br /><br />
-<div class="container">
+@section('title')
+<img src="/icon/seta.svg" height="30" width="25" style="margin-top: -3px;"/>
+Dashboard
+@endsection
+<div class="mui-container">
     @if (session('status'))
         <div class="alert alert-success">
             {{ session('status') }}
         </div>
     @endif
-    <?php    
-    header("Location: ../index.php");exit;   
-    ?>
+    <div class="row">
+        <div class="mui-col-md-8 mui-col-md-offset-2">
+            <div class="mui-panel">
+                <div class="panel-heading">Bem-vindo!</div>
+                <div class="panel-body">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
