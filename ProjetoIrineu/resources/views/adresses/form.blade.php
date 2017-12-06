e186cd1c7c4aa743507582a70a1300305495940d
denius72 <denius72@gmail.com>
2017-12-06 08:37:16 -0200

    Adicionado princípio do formulário do Endereço

---
+++ ProjetoIrineu/resources/views/adresses/form.blade.php
@@ -0,0 +1,143 @@
+@extends('layouts.app')
+<br /><br /><br /><br />
+@section('title')
+<img src="/icon/seta.svg" height="30" width="25" style="margin-top: -3px;"/>
+Endereços
+@endsection
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="mui-panel">
+
+                <div class="panel-heading">
+                    Informe abaixo as informações do endereço
+                </div>
+
+                <div class="panel-body">
+                @if(isset($errors) && count($errors)>0)
+                <div class= "alert alert-danger">
+                
+                @foreach($errors->all() as $error)
+                <p>{{$error}}</p>
+                @endforeach
+                </div>
+              @endif
+              @if(Session::has('mensagem_sucesso'))
+                <div class= "alert alert-success">{{ Session::get('mensagem_sucesso')}}</div>
+              @endif
+
+
+                @if(Request::is('*/edit'))
+                      <!-- editando -->
+                      {{Form::model($selectiveprocesses, ['method' => 'PATCH','url' => 'selectiveprocesses/'.$selectiveprocesses->id])}}
+                @else
+                     <!-- incluindo -->
+
+                    {{ Form::open(['route' => 'selectiveprocesses.store']) }}
+                @endif
+
+                <div class="mui-textfield mui-textfield--float-label">
+                    {{ form::input('text','name',null)}}
+                    <label>Nome</label>
+                </div>
+
+                <div class="mui-textfield mui-textfield">
+                {{ form::date('start_date',null,['class' => '', 'autofocus'])}}
+                 
+                     <label>Data de Inicio</label>
+                </div>
+
+                <div class="mui-textfield mui-textfield">
+                {{ form::date('end_date',null,['class' => '', 'autofocus'])}}
+                 
+                     <label>Data de Fim</label>
+                </div>
+
+                <div class="mui-textfield mui-textfield--float-label">
+                    {{ form::input('text','description')}}
+                    <label>Descrição</label>
+                </div>
+
+                <div class="form-group col-md-2 ">
+                    {{ form::checkbox('active',true)}}
+                    <span style="margin-left: 10px;">{{ form::label('active','Ativo')}}</span>
+                </div>             
+                <table class="mui-table ">
+                <tbody>
+                <thead>
+                    <tr>
+                        <th>
+                            Cursos
+                        </th>
+                    </tr>
+                </thead>
+                @if ($courses->count() == 0)
+                        <tr> 
+                            <p>não há cursos cadastrados</p>
+                        </tr>  
+                @else
+                    @foreach ($courses as $course)                
+                        <tr>
+                            <td class="">
+                            
+                                <span>{{ form::checkbox("course[$course->id][id]",$course->id,null,['class' => 'cb', 'id' => ''.$course->name])}}</span>
+                                <span style="margin-left: 10px;">{{ form::label('name',$course->name)}}</span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="mui-textfield mui-textfield--float-label">
+
+                                {{ form::input('number',"course[$course->id][vacancy]",null,['autofocus'])}}
+                                        
+                                <label>Vagas</label>
+                            </td>
+                        </tr> 
+                    @endforeach
+                @endif
+                </tbody>
+                </table>
+                <table class="mui-table">
+                <tbody>
+                <thead>
+                    <tr>
+                        <th>
+                            Cotas
+                        </th>
+                    </tr>
+                </thead>
+                @if ($quotas->count() == 0)
+                    <tr class=""> 
+                        <p>não há cursos cadastrados</p>
+                    </tr>    
+                @else
+                    @foreach ($quotas as $quotas)                
+                        <tr>
+                            <td>
+                                {{ form::checkbox("quotas[$quotas->id][id]",$quotas->id,null,['class' => 'cb', 'id' => ''.$quotas->name])}}
+                                <span style="margin-left: 10px;">{{ form::label('name',$quotas->name)}}</span>
+                            </td> 
+                        </tr> 
+                        <tr>    
+                            <td class="mui-textfield mui-textfield--float-label">
+                                {{ form::input('number',"quotas[$quotas->id][vacancy]",null)}}
+                                
+                                <label>Vagas</label>
+                            </td>
+                        </tr>
+                    @endforeach
+                @endif
+                </tbody>
+                </table>
+               <!-- btn voltar -->
+               <div style="margin-left: calc(50% - 115px);">
+               <a class="mui-btn mui-btn--raised" href="{{url('selectiveprocesses')}}">Voltar</a>
+               <button type="submit" class="mui-btn mui-btn--primary">Salvar</button>
+                 </div>
+     
+                {{ Form::close() }}              
+                </form>  
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ ProjetoIrineu/resources/views/adresses/list.blade.php
@@ -0,0 +1,78 @@
+@extends('layouts.app')
+<br /><br /><br /><br />
+@section('title')
+<img src="/icon/seta.svg" height="30" width="25" style="margin-top: -3px;"/>
+Endereços
+@endsection
+<div class="mui-container">
+    <div class="row">
+    <div style="width: 50%; left:25%; right: 75%; position: absolute;">
+    
+                <div class="panel-body">
+
+
+                @if(Session::has('mensagem_sucesso'))
+                  <div class= "alert alert-success">{{ Session::get('mensagem_sucesso')}}</div>
+                @endif
+                    <div style="margin-left: calc(30% - 150px);">
+                            <table class ="mui-table" style="margin-top: 0px;">
+                                <thead style="border-bottom: 2px solid #E0E0E0;">
+                                    <tr>
+                                        <th>
+                                            <div style="margin-top: 20px;">
+                                                ID
+                                            </div>
+                                        </th>
+                                        <th>
+                                            <div style="margin-top: 20px;">
+                                                Nome
+                                            </div>
+                                        </th>
+                                        <th>
+                                            <div style="margin-top: 20px;">
+                                                Início
+                                            </div>
+                                        </th>
+                                        <th>
+                                            <div style="margin-top: 20px;">
+                                                Fim
+                                            </div>
+                                        </th>
+                                        <th>
+                                            <div style="margin-top: 20px; margin-left: calc(100% - 55px);">
+                                                Ações
+                                            </div>
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($selectiveprocesses as $selectiveprocess)
+                                        <tr style="border-top: 1px solid #E0E0E0; border-collapse: separate;">
+                                        <td>{{ $selectiveprocess->id }}</td>
+                                        <td>{{ $selectiveprocess->name }}</td>
+                                        <td>{{ $selectiveprocess->start_date }}</td>
+                                        <td>{{ $selectiveprocess->end_date }}</td>
+                                            <td>
+                                                <form action="selectiveprocesses/{{ $selectiveprocess->id }}/edit" style="margin-top: 10px; margin-left: calc(100% - 55px);">
+                                                    <button class="mui-btn mui-btn--small mui-btn--raised mui-btn--primary" style="display: inline; font-size: 25;"><img src="/icon/ic_mode_edit_white_24px.svg" height="20" width="20"/></button>
+                                                </form>    
+                                            </td> 
+                                            <td>                
+                                                <form action="selectiveprocesses/{{ $selectiveprocess->id }}/confirmDestroy" style="margin-top: 10px; margin-left: calc(10% - 25px);">
+                                                    <button class="mui-btn mui-btn--small mui-btn--raised mui-btn--danger" style="display: inline; font-size: 20;"><img src="/icon/ic_delete_forever_white_24px.svg" height="20" width="20" /></button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                    </div>
+                </div>
+        </div>
+    </div>
+</div>
+<form action="{{url('selectiveprocesses/create')}}" style="margin-left: 100px; margin-bottom: -20px;">
+    <div class="fixed-action-btn horizontal click-to-toggle">
+        <button class="mui-btn mui-btn--fab mui-btn--primary" style="font-size: 25px;">+</button>
+    </div>
+</form>
